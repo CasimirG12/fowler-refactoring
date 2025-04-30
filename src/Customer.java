2096462b1b4037b0504a2436faf84f3b2e15ea9c
CasimirG12 <casimirbomans@gmail.com>
2025-04-30 15:26:07 +0200

    dispencables: removed temp variable totalamount from statement method

--- src/Customer.java
+++ src/Customer.java
@@ -17,7 +17,6 @@ class Customer {
     };
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration<Rental> enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
@@ -31,11 +30,20 @@ class Customer {
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
                     + String.valueOf(thisAmount) + "\n";
-            totalAmount += each.getCharge();
         }
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration<Rental> enum_rentals = rentals.elements();
+        while (enum_rentals.hasMoreElements()) {
+            Rental each = (Rental) enum_rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 }
